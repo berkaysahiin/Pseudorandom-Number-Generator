d2abba0275106e136263d04be9c26a2ca412c274
Berkay Sahin <92673021+berkaysahiin@users.noreply.github.com>
2024-10-10 17:57:12 +0300

    Update random_number_generator.h

--- random_number_generator/random_number_generator.h
+++ random_number_generator/random_number_generator.h
@@ -1,8 +1,13 @@
 #include <stdint.h>
 
+/* Macro definitions for bit manipulation */
+#define SET_BIT(target, index) (target |= (1U << (index)))
+#define CLEAR_BIT(target, index) (target &= ~(1U << (index)))
+
+/* Random number generator structure */
 typedef struct {
-  uint32_t state;
-}random_number_generator;
+    uint32_t state;
+} random_number_generator;
 
 /* --------------PUBLIC-------------------*/
 uint32_t get_next(random_number_generator* rng);
@@ -13,122 +18,106 @@ uint32_t xor_shift(uint32_t value);
 uint32_t revert(uint32_t value);
 
 /* --------------PRIVATE-------------------*/
-uint32_t revert_4_bits_left_shift(uint32_t xorshiftedValue);
-uint32_t revert_17_bits_right_shift(uint32_t xorshiftedValue);
-uint32_t revert_13_bits_left_shift(uint32_t xorshiftedValue);
+uint32_t revert_bits_shift(uint32_t xorshiftedValue, int shift, int is_left);
 void set_bit(uint32_t* target, int val, int index);
-int get_bit(uint32_t target, int index) ;
-int xor(int b1, int b2);
+int get_bit(const uint32_t target, int index);
+int xor(const int b1, const int b2);
 
-/*------------------------------------------------------------------------------------*/
+/* --------------------------------------------------------------------------------- */
 
-random_number_generator init(uint32_t seed)
-{
-  random_number_generator rng = {
-    .state = seed
-  };
-
-  return rng;
+random_number_generator init(uint32_t seed) {
+    random_number_generator rng = {
+        .state = seed
+    };
+    return rng;
 }
 
-uint32_t get_next(random_number_generator* rng)
-{
-  rng->state = xor_shift(rng->state);
-  return rng->state;
+uint32_t get_next(random_number_generator* rng) {
+    rng->state = xor_shift(rng->state);
+    return rng->state;
 }
 
-uint32_t get_next_range(random_number_generator* rng, int upper_bound)
-{
-  return get_next(rng) % upper_bound;
+uint32_t get_next_range(random_number_generator* rng, int upper_bound) {
+    if (upper_bound <= 0) {
+        return 0; 
+    }
+    return get_next(rng) % upper_bound;
 }
 
-uint32_t get_prev(random_number_generator* rng)
-{
-  rng->state = revert(rng->state);
-  return rng->state;
+uint32_t get_prev(random_number_generator* rng) {
+    rng->state = revert(rng->state);
+    return rng->state;
 }
 
-uint32_t get_prev_range(random_number_generator* rng, int upper_bound)
-{
-  return get_prev(rng) % upper_bound;
+uint32_t get_prev_range(random_number_generator* rng, int upper_bound) {
+    if (upper_bound <= 0) {
+        return 0; 
+    }
+    return get_prev(rng) % upper_bound;
 }
 
-uint32_t xor_shift(uint32_t value) 
-{
-  value ^= value << 13;
-  value ^= value >> 17;
-  value ^= value << 4;
-
-  return value;
+uint32_t xor_shift(uint32_t value) {
+    value ^= value << 13;
+    value ^= value >> 17;
+    value ^= value << 4;
+    return value;
 }
 
-uint32_t revert(uint32_t value)
-{
-  return revert_13_bits_left_shift(revert_17_bits_right_shift(revert_4_bits_left_shift(value))); 
+uint32_t revert(uint32_t value) {
+    value = revert_bits_shift(value, 4, 1);   // 4-bit left shift revert
+    value = revert_bits_shift(value, 17, 0);  // 17-bit right shift revert
+    value = revert_bits_shift(value, 13, 1);  // 13-bit left shift revert
+    return value;
 }
 
-//----------------------------------BITWISE.OPERATIONS-------------------------------------------------------------------------------
+/**
+ * @brief Reverts an arbitrary shift (left or right) by a specified number of bits.
+ *
+ * @param xorshiftedValue The value to revert.
+ * @param shift The number of bits that were shifted.
+ * @param is_left Boolean flag indicating if it was a left shift (1) or right shift (0).
+ * @return The reverted value.
+ */
+uint32_t revert_bits_shift(uint32_t xorshiftedValue, int shift, int is_left) {
+    uint32_t revertedValue = 0;
 
-void set_bit(uint32_t* target, int val, int index)
-{
-  if(val == 1) {
-    *target |= 1U << index;
-  }
-  else {
-    *target &= ~(1U << index);
-  }
+    if (is_left) {
+      
+        for (int i = 0; i < shift; i++) {
+            set_bit(&revertedValue, get_bit(xorshiftedValue, i), i);
+        }
+      
+        for (int i = shift; i < 32; i++) {
+            set_bit(&revertedValue, xor(get_bit(revertedValue, i - shift), get_bit(xorshiftedValue, i)), i);
+        }
+    } else {
+      
+        for (int i = 32 - shift; i < 32; i++) {
+            set_bit(&revertedValue, get_bit(xorshiftedValue, i), i);
+        }
+
+        for (int i = 0; i < 32 - shift; i++) {
+            set_bit(&revertedValue, xor(get_bit(revertedValue, i + shift), get_bit(xorshiftedValue, i)), i);
+        }
+    }
+
+    return revertedValue;
 }
 
-int get_bit(uint32_t target, int index) 
-{
-  return (target & (1 << index)) != 0;
+/* ------------------------- Bitwise Operations ---------------------------- */
+
+inline void set_bit(uint32_t* target, int val, int index) {
+    if (val == 1) {
+        SET_BIT(*target, index);
+    } else {
+        CLEAR_BIT(*target, index);
+    }
 }
 
-int xor(int b1, int b2) {
+inline int get_bit(const uint32_t target, int index) {
+    return (target & (1 << index)) != 0;
+}
+
+inline int xor(const int b1, const int b2) {
     return !(b1 == b2);
 }
-
-uint32_t revert_4_bits_left_shift(uint32_t xorshiftedValue) 
-{
-  uint32_t revertedValue = 0;
-  for(int i=0; i<4; i++) {
-    set_bit(&revertedValue,get_bit(xorshiftedValue,i),i);
-  }
-  
-  for(int i=4; i<32; i++) {
-    set_bit(&revertedValue, xor(get_bit(revertedValue, i-4), get_bit(xorshiftedValue, i)), i);
-  }
-    
-  return revertedValue;
-}
-
-uint32_t revert_17_bits_right_shift(uint32_t xorshiftedValue) 
-{
-  uint32_t revertedValue = 0;
-
-  for(int i=15; i<32; i++) {
-    set_bit(&revertedValue,get_bit(xorshiftedValue,i), i);
-  }
-
-  for(int i=0; i<15; i++) {
-    set_bit(&revertedValue, xor(get_bit(revertedValue, i+17), get_bit(xorshiftedValue, i)), i);
-  }
-  
-  return revertedValue;
-}
-
-uint32_t revert_13_bits_left_shift(uint32_t xorshiftedValue) 
-{
-  uint32_t revertedValue = 0;
-
-  for(int i=0; i<13; i++) {
-    set_bit(&revertedValue, get_bit(xorshiftedValue,i), i);
-  }
-
-  for(int i=13; i<32; i++) {
-    set_bit(&revertedValue, xor(get_bit(revertedValue, i - 13), get_bit(xorshiftedValue, i)), i);
-  }
-  
-  return revertedValue;
-}
-
